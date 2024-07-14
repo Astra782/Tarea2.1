67a1e25f200ef10e74041cf6fc4be3bbb05ab6b3
Roberto Josue Orellana <159511699+Astra782@users.noreply.github.com>
2024-07-13 23:28:17 -0600

    Add files via upload

---
+++ lib/screens/welcomeScreen.dart
@@ -0,0 +1,77 @@
+import 'package:app_de_autenticacion/screens/signinScreen.dart';
+import 'package:app_de_autenticacion/screens/signupScreen.dart';
+import 'package:app_de_autenticacion/widgets/customScaffold.dart';
+import 'package:app_de_autenticacion/widgets/welcomeBTN.dart';
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScaffold(
+      child: Column(
+        children: [
+          Flexible(
+              flex: 8,
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 0,
+                  horizontal: 40.0,
+                ),
+                child: Center(
+                  child: RichText(
+                    textAlign: TextAlign.center,
+                    text: const TextSpan(
+                      children: [
+                        TextSpan(
+                            text: '¡Bienvenido!\n',
+                            style: TextStyle(
+                              fontSize: 45.0,
+                              fontWeight: FontWeight.w600,
+                            )),
+                        TextSpan(
+                            text:
+                                '\nIngrese datos personales',
+                            style: TextStyle(
+                              fontSize: 20,
+                              // height: 0,
+                            ))
+                      ],
+                    ),
+                  ),
+                ),
+              )),
+          const Flexible(
+            flex: 2,
+            child: Align(
+              alignment: Alignment.bottomRight,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: WelcomeButton(
+                      buttonText: 'Iniciar sesión',
+                      onTap: SignInScreen(),
+                      //Sign in, accion
+                      color: Colors.transparent,
+                      textColor: Colors.white,
+                    ),
+                  ),
+                  Expanded(
+                    child: WelcomeButton(
+                      buttonText: 'Registrarse',
+                      onTap:  SignUpScreen(),
+                      //Sign up, accion
+                      color: Colors.grey,
+                      textColor:  Color.fromARGB(255, 0, 0, 0),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
